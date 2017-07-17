4156fa0c62d702360e7a5a6f4942c1bbba227088
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-07-17 18:28:51 +0800

    :bookmark: 2.0.0

--- index.spec.js
+++ index.spec.js
@@ -1,42 +1,55 @@
 const sectionListGetItemLayout = require('./dist').default
 
 test('Empty sections', () => {
-  const getItemLayout = sectionListGetItemLayout(
-    () => 3,
-    () => 5,
-    () => 7,
-  )
-  const data = [
-    { data: [] },
-    { data: [] },
-    { data: [null] },
-  ]
-  expect(getItemLayout(data, 0)).toEqual({ length: 3, offset: 0, index: 0 })
-  expect(getItemLayout(data, 2)).toEqual({ length: 3, offset: 6, index: 2 })
+  const getItemLayout = sectionListGetItemLayout({
+    getItemHeight: () => 2,
+    getSeparatorHeight: () => 23,
+    getSectionHeaderHeight: () => 41,
+    getSectionFooterHeight: () => 61,
+  })
+
+  const data = [{ data: [] }, { data: [] }, { data: [null] }]
+
+  expect(getItemLayout(data, 0)).toEqual({ length: 41, offset: 0, index: 0 })
+  expect(getItemLayout(data, 1)).toEqual({ length: 61, offset: 41, index: 1 })
+  expect(getItemLayout(data, 5)).toEqual({
+    length: 2,
+    offset: 41 * 3 + 61 * 2,
+    index: 5,
+  })
+  expect(getItemLayout(data, 6)).toEqual({
+    length: 61,
+    offset: 41 * 3 + 61 * 2 + 2,
+    index: 6,
+  })
 })
 
-test('First row', () => {
-  const getItemLayout = sectionListGetItemLayout(
-    () => 3,
-    () => 5,
-    () => 7,
-  )
-  const data = [
-    { data: [null, null, null] },
-    { data: [null, null, null] },
-  ]
-  expect(getItemLayout(data, 1)).toEqual({ length: 7, offset: 3, index: 1 })
+test('Multiple rows in one section', () => {
+  const getItemLayout = sectionListGetItemLayout({
+    getItemHeight: () => 2,
+    getSeparatorHeight: () => 23,
+    getSectionHeaderHeight: () => 41,
+    getSectionFooterHeight: () => 61,
+  })
+
+  const data = [{ data: [null, null, null] }]
+
+  expect(getItemLayout(data, 2)).toEqual({
+    length: 2,
+    offset: 41 + 2 + 23,
+    index: 2,
+  })
 })
 
-test('Last row', () => {
-  const getItemLayout = sectionListGetItemLayout(
-    () => 3,
-    () => 5,
-    () => 7,
-  )
-  const data = [
-    { data: [null] },
-    { data: [null, null] },
-  ]
-  expect(getItemLayout(data, 4)).toEqual({ length: 7, offset: 25, index: 4 })
+test('Calling sectionListGetItemLayout with only getItemHeight', () => {
+  const getItemLayout = sectionListGetItemLayout({
+    getItemHeight: () => 1,
+  })
+
+  const data = [{ data: [null, null] }]
+
+  expect(getItemLayout(data, 0)).toEqual({ length: 0, offset: 0, index: 0 })
+  expect(getItemLayout(data, 1)).toEqual({ length: 1, offset: 0, index: 1 })
+  expect(getItemLayout(data, 2)).toEqual({ length: 1, offset: 1, index: 2 })
+  expect(getItemLayout(data, 3)).toEqual({ length: 0, offset: 2, index: 3 })
 })
--- index.ts
+++ index.ts
@@ -1,40 +1,83 @@
-export type SectionListDataProp = {
+export type SectionListDataProp = Array<{
   title: string
   data: any[]
-}[]
+}>
 
-export default (
-  getSectionHeaderHeight: (sectionIndex: number) => number,
-  getSeparatorHeight: (sectionIndex: number, rowIndex: number) => number,
-  getItemHeight: (rowData: any, sectionIndex: number, rowIndex: number) => number,
-) => (data: SectionListDataProp, index: number) => {
-  let i = 0, sectionIndex = 0, rowIndex = null, offset = 0
+interface SectionHeader {
+  type: 'SECTION_HEADER'
+}
+
+interface Row {
+  type: 'ROW'
+  index: number
+}
+
+interface SectionFooter {
+  type: 'SECTION_FOOTER'
+}
+
+type ListElement = SectionHeader | Row | SectionFooter
+
+export interface Parameters {
+  getItemHeight: (
+    rowData: any,
+    sectionIndex: number,
+    rowIndex: number,
+  ) => number
+  getSeparatorHeight?: (sectionIndex: number, rowIndex: number) => number
+  getSectionHeaderHeight?: (sectionIndex: number) => number
+  getSectionFooterHeight?: (sectionIndex: number) => number
+}
+
+export default ({
+  getItemHeight,
+  getSeparatorHeight = () => 0,
+  getSectionHeaderHeight = () => 0,
+  getSectionFooterHeight = () => 0,
+}: Parameters) => (data: SectionListDataProp, index: number) => {
+  let i = 0
+  let sectionIndex = 0
+  let elementPointer: ListElement = { type: 'SECTION_HEADER' }
+  let offset = 0
 
   while (i < index) {
-    // If rowIndex is null, we are currently pointing at this section's section header
-    if (rowIndex === null) {
-      offset += getSectionHeaderHeight(sectionIndex)
+    switch (elementPointer.type) {
+      case 'SECTION_HEADER': {
+        const sectionData = data[sectionIndex].data
 
-      const sectionData = data[sectionIndex].data
-      // If this section is empty, we go right to the next section...
-      if (sectionData.length === 0) {
-        sectionIndex += 1
-      // ...otherwise we make rowIndex point at the first row in this section
-      } else {
-        rowIndex = 0
+        offset += getSectionHeaderHeight(sectionIndex)
+
+        // If this section is empty, we go right to the footer...
+        if (sectionData.length === 0) {
+          elementPointer = { type: 'SECTION_FOOTER' }
+          // ...otherwise we make elementPointer point at the first row in this section
+        } else {
+          elementPointer = { type: 'ROW', index: 0 }
+        }
+
+        break
       }
-    } else {
-      const sectionData = data[sectionIndex].data
-      offset += getItemHeight(sectionData[rowIndex], sectionIndex, rowIndex)
-      rowIndex += 1
+      case 'ROW': {
+        const sectionData = data[sectionIndex].data
 
-      if (rowIndex >= sectionData.length) {
+        const rowIndex = elementPointer.index
+
+        offset += getItemHeight(sectionData[rowIndex], sectionIndex, rowIndex)
+        elementPointer.index += 1
+
+        if (rowIndex === sectionData.length - 1) {
+          elementPointer = { type: 'SECTION_FOOTER' }
+        } else {
+          offset += getSeparatorHeight(sectionIndex, rowIndex)
+        }
+
+        break
+      }
+      case 'SECTION_FOOTER': {
+        offset += getSectionFooterHeight(sectionIndex)
         sectionIndex += 1
-        rowIndex = null
-      // If this is not the last row in the section, we have to account for the
-      // row separator
-      } else {
-        offset += getSeparatorHeight(sectionIndex, rowIndex)
+        elementPointer = { type: 'SECTION_HEADER' }
+        break
       }
     }
 
@@ -42,11 +85,22 @@ export default (
   }
 
   let length
-  if (rowIndex === null) {
-    length = getSectionHeaderHeight(sectionIndex)
-  } else {
-    length = getItemHeight(data[sectionIndex].data[rowIndex], sectionIndex, rowIndex)
+  switch (elementPointer.type) {
+    case 'SECTION_HEADER':
+      length = getSectionHeaderHeight(sectionIndex)
+      break
+    case 'ROW':
+      const rowIndex = elementPointer.index
+      length = getItemHeight(
+        data[sectionIndex].data[rowIndex],
+        sectionIndex,
+        rowIndex,
+      )
+      break
+    case 'SECTION_FOOTER':
+      length = getSectionFooterHeight(sectionIndex)
+      break
   }
-  
+
   return { length, offset, index }
 }
